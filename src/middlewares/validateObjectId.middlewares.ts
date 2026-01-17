63935c3804cd792cefc8e943c5255eadf1f93c00
stratossalpas <stratossalpas@aueb.gr>
2026-01-17 16:49:43 +0200

    fix: validateObjectmiddlewares for typescript errors

--- src/middlewares/validateObjectId.middlewares.ts
+++ src/middlewares/validateObjectId.middlewares.ts
@@ -2,22 +2,15 @@ import { Request, Response, NextFunction } from "express";
 import mongoose from "mongoose";
 
 export const validateObjectId = (paramName: string = 'id') => (req: Request, res: Response, next: NextFunction) => {
-  const paramValue = req.params[paramName];
-  
-  // Έλεγχος αν υπάρχει
+  let paramValue = req.params[paramName];
+  if (Array.isArray(paramValue)) paramValue = paramValue[0];
   if (!paramValue) {
-    return res.status(400).json({message: `Missing parameter: ${paramName}`});
+    return res.status(400).json({ message: `Missing parameter: ${paramName}` });
   }
-  
-  // Έλεγχος αν είναι array
-  if (Array.isArray(paramValue)) {
-    return res.status(400).json({message: `Parameter ${paramName} should not be an array`});
-  }
-  
-  // Έλεγχος αν είναι valid ObjectId
   if (!mongoose.Types.ObjectId.isValid(paramValue)) {
-    return res.status(400).json({message: "Not correct ObjectId"});
+    return res.status(400).json({ message: "Not correct ObjectId" });
   }
-  
+  req.params[paramName] = paramValue;
+
   next();
 };
